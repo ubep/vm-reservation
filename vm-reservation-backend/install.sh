863f5df868888cfafb8ec34168db907c535ead70
ubep <u.biallas@epages.com>
2015-07-08 16:32:04 +0200

    added npm install to setup file

--- vm-reservation-backend/install.sh
+++ vm-reservation-backend/install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+npm install
+
 node setup_db.js
 
 export SERVERPATH=$(pwd)'/index.js'
 sed "s=SERVERPLACEHOLDER=$SERVERPATH=g" vmtool.tmp > vmtool
 
 cp vmtool /etc/init.d/vmtool
-cd /etc/init.d/
-chmod 777 vmtool
+chmod 777 /etc/init.d/vmtool
 
 service vmtool start
