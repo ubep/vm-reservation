0c0b4b378fcc092bb0a8a3f91c529de5d93a4fe7
ubep <u.biallas@epages.com>
2015-07-16 15:34:21 +0200

    added backup script

--- vm-reservation-backend/install.sh
+++ vm-reservation-backend/install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+mkdir backup
+
 npm install
 
 node setup_db.js
 
 export SERVERPATH=$(pwd)
 sed "s=SERVERPLACEHOLDER=$SERVERPATH=g" vmtool.tmp > vmtool
+sed "s=SERVERPLACEHOLDER=$SERVERPATH=g" backup.tmp > backup.sh
 
 cp vmtool /etc/init.d/vmtool
 chmod 777 /etc/init.d/vmtool
