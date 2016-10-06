644d448e44ed212f4d8f44727094d21eb15dfd74
Thomas Hirsch <thomas.hirsch@epages.com>
2016-10-06 14:16:28 +0200

    remove backup

--- vm-reservation-backend/install.sh
+++ vm-reservation-backend/install.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-
-mkdir backup
-
 npm install
 
 node setup_db.js
 
 export SERVERPATH=$(pwd)
 sed "s=SERVERPLACEHOLDER=$SERVERPATH=g" vmtool.tmp > vmtool
-sed "s=SERVERPLACEHOLDER=$SERVERPATH=g" backup.tmp > backup.sh
 
 mv vmtool /etc/init.d/vmtool
 chmod 777 /etc/init.d/vmtool
