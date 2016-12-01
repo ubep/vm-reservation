ef5e1e5469e7597914e8a2f1398542bdd74c8883
ubep <u.biallas@epages.com>
2016-12-01 18:22:26 +0100

    start with new vm tool ui

---
+++ vm-reservation-ui/src/VMTool.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class VMTool extends React.Component {
+
+  render() {
+    return (
+      <div>VM Tool</div>
+    )
+  }
+
+}
---
+++ vm-reservation-ui/src/main.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import VMTool from './VMTool.jsx'
+
+ReactDOM.render(<VMTool/>, document.getElementById('vmtool'))
